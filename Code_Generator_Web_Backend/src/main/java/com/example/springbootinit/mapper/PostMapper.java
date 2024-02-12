b30806a405f4a76583de1bd60a887b073a3397b7
zwy123zwy <81016312+zwy123zwy@users.noreply.github.com>
2024-02-12 13:37:44 +0800

    第5次提交

---
+++ Code_Generator_Web_Backend/src/main/java/com/example/springbootinit/mapper/PostMapper.java
@@ -0,0 +1,15 @@
+package com.example.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.springbootinit.model.entity.Post;
+
+/**
+ * 帖子数据库操作
+ */
+public interface PostMapper extends BaseMapper<Post> {
+
+}
+
+
+
+
